0e26556a43e5ae75ea8eba21c2465f4c9102f4a4
TylerSnow-M <tylersnow02@gmail.com>
2023-07-21 12:25:59 -0400

    added voters route

--- server.js
+++ server.js
@@ -25,71 +25,8 @@ db.connect(err => {
     console.log(`Server running on port ${PORT}`);
   });
 });
-// Get all parties
-app.get("/api/parties", (req, res) => {
-  const sql = `SELECT * FROM parties`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: rows,
-    });
-  });
-});
-
-// Get single party
-app.get("/api/party/:id", (req, res) => {
-  const sql = `SELECT * FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-
-  db.query(sql, params, (err, row) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: "success",
-      data: row,
-    });
-  });
-});
-
-// Delete a party
-app.delete("/api/party/:id", (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      // checks if anything was deleted
-    } else if (!result.affectedRows) {
-      res.json({
-        message: "Party not found",
-      });
-    } else {
-      res.json({
-        message: "deleted",
-        changes: result.affectedRows,
-        id: req.params.id,
-      });
-    }
-  });
-});
 
 // Not Found response for unmatched routes
 app.use((req, res) => {
   res.status(404).end();
-});
-
-// Start server after DB connection
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Database connected.");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
 });
